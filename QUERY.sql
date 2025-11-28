88bb0808c72f77fb04d869346e13fa6e48dfbefe
Estefania Sanchez <estefania_sanchez@javeriana.edu.co>
2025-11-28 17:53:56 -0500

    Delete QUERY.sql

--- QUERY.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    map_code,
-    AVG(
-        EXTRACT(HOUR FROM (endtime - starttime)) * 3600 +
-        EXTRACT(MINUTE FROM (endtime - starttime)) * 60 +
-        EXTRACT(SECOND FROM (endtime - starttime))
-    ) AS avg_duration_seconds
-FROM Game
-GROUP BY map_code;
